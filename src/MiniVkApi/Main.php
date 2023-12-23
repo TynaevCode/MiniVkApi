1ee2244da629cca4388330332407706c6850d861
Aziret Asynchronovich <154589926+Az1koGH@users.noreply.github.com>
2023-12-23 12:25:10 +0600

    Update Main.php

--- src/MiniVkApi/Main.php
+++ src/MiniVkApi/Main.php
@@ -12,13 +12,6 @@ class Main extends PluginBase {
     /** @var int */
     public static $con_timeout;
     
-    /** @var Main */
-    private static $instance;
-    
-    function onLoad() {
-        self::$instance = $this;
-    }
-    
     function onEnable() : void {
         $this->saveDefaultConfig();
         self::$timeout = $this->getConfig()->get("timeout");
@@ -32,13 +25,6 @@ class Main extends PluginBase {
      */
     public function newClient(string $token) : VkApiClient {
         return new VkApiClient($token);
-    }
-    
-    /**
-     * @return Main
-     */
-    public static function getInstance() : Main {
-        return self::$instance;
     }
     
 }
